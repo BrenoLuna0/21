8c63787aecbd766841763650edb0fd80b4c6f416
Breno Luna <breno.luna_@hotmail.com>
2018-05-03 08:42:15 -0300

    Implementação do sistema de estados

---
+++ Parada.java
@@ -0,0 +1,34 @@
+
+public class Parada implements PlayerState{
+
+	
+	public void jogavel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void estourada() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void blackjack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void modificada() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void execute(Jogador j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
