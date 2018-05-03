744e2f412626bb5876811b7e839d8bdb0026ab11
Breno Luna <breno.luna_@hotmail.com>
2018-05-03 08:39:51 -0300

    Implementação do sistema de estados

---
+++ HandListener.java
@@ -0,0 +1,7 @@
+
+public interface HandListener {
+	public void jogavel(Participante p);
+	public void estourada(Participante p);
+	public void blackjack(Participante p);
+	public void modificada(Mao m, Participante p);
+}
